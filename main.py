16d32c208184e9300f2765fc1a78ea4bbfc34a94
Victor Sanahuja <piscue@gmail.com>
2019-03-11 22:43:47 +0100

    add flashing

--- main.py
+++ main.py
@@ -39,32 +39,41 @@ def resetTimer(time):
     return status, actualTime
 
 
+def flashing():
+    for x in range(0, 5):
+        for y in range(0, 5):
+            if display.get_pixel(x, y) == 7:
+                display.set_pixel(x, y, 9)
+            else:
+                if display.get_pixel(x, y) == 9:
+                    display.set_pixel(x, y, 7)
+
+
+
 status = resetTimer(pomodoroSlot)
 seconds = 60
 # create a loop that don't block button input and allows to update the timer
 while True:
     if button_a.is_pressed():
-        print("button a pressed")
         if status[0] == "running":
             status = pauseTime()
         if status[0] == "paused":
             status = resetTimer(pomodoroSlot)
         if status[0] == "reset":
-            print("status reset")
             status = startTimer(pomodoroSlot)
             actualTime = status[1]
     # print(status)
     if status[0] == "running":
         if actualTime <= pomodoroSlot and actualTime > -1:
-            seconds = seconds + 1
-            print(seconds)
             if seconds == 60:
                 actualTime = actualTime - 1
                 seconds = 0
-            print(actualTime)
+                printTime = imageNumber(actualTime)
+                display.show(printTime)
+            seconds = seconds + 1
+            # print(seconds)
+            flashing()
             # if image contains 7 replace by 9 else replace by 7
-            printTime = imageNumber(actualTime)
-            display.show(printTime)
             sleep(1000)
         else:
             print(actualTime)
