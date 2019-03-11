9bc78b035ec73d8c2ba0852aa48faa25d5c01dad
Victor Sanahuja <piscue@gmail.com>
2019-03-11 20:24:28 +0100

    timer works

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from microbit import *
 # 25 minutes per pomodoro
 pomodoroSlot = 25
 
-global status
+global status, seconds
 
 
 def imageNumber(number):
@@ -153,36 +153,58 @@ def imageNumber(number):
 def startTimer(time):
     status = "running"
     display.scroll("3 2 1 GO")
-    for i in range(0, time):
-        time = time - 1
-        display.show(imageNumber(time))
-        sleep(500)
-    display.show(Image.HAPPY)
-    status = "paused"
-    return status
+    # for i in range(0, time):
+    #     time = time - 1
+    #     display.show(imageNumber(time))
+    #     sleep(500)
+    # display.show(Image.HAPPY)
+    # status = "paused"
+    return status, time
 
 
 # press button again pause the timer
 def pauseTime():
     status = "paused"
-    display.scroll("Pause")
-    return status
+    actualTime = ""
+    return status, actualTime
 
 
 # press button again will reset the time
 def resetTimer(time):
     status = "reset"
     display.show(imageNumber(time))
-    return status
+    actualTime = time
+    return status, actualTime
 
 
 status = resetTimer(pomodoroSlot)
-
+seconds = 60
+# create a loop that don't block button input and allows to update the timer
 while True:
     if button_a.is_pressed():
-        if status == "running":
+        print("button a pressed")
+        if status[0] == "running":
             status = pauseTime()
-        if status == "paused":
+        if status[0] == "paused":
             status = resetTimer(pomodoroSlot)
-        if status == "reset":
+        if status[0] == "reset":
+            print("status reset")
             status = startTimer(pomodoroSlot)
+            actualTime = status[1]
+    # print(status)
+    if status[0] == "running":
+        if actualTime <= pomodoroSlot and actualTime > -1:
+            seconds = seconds + 1
+            print(seconds)
+            if seconds == 60:
+                actualTime = actualTime - 1
+                seconds = 0
+            print(actualTime)
+            # if image contains 7 replace by 9 else replace by 7
+            printTime = imageNumber(actualTime)
+            display.show(printTime)
+            sleep(1000)
+        else:
+            print(actualTime)
+            display.show(Image.HAPPY)
+            status = "paused"
